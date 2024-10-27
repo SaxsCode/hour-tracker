dff86a555f29e64f8094dc4299351cb9d798fa3b
SaxsCode <maxsax.sch@gmail.com>
2024-10-27 13:26:11 +0100

    feat: show end time in overview

--- src/watch.php
+++ src/watch.php
@@ -9,31 +9,31 @@ use Spatie\Watcher\Watch;
 define("FILE_NAME", $settings['LOG_DIR'] . "\\" . date('Y-m-d'));
 
 if (empty($settings['DIRECTORY'])) {
-    echo "SET THE DIRECTORY YOU WANT TO WATCH IN settings.php" . PHP_EOL;
-    exit(1);
+  echo "SET THE DIRECTORY YOU WANT TO WATCH IN settings.php" . PHP_EOL;
+  exit(1);
 }
 
 echo "WATCHING..", PHP_EOL;
 
 Watch::path($settings['DIRECTORY'])
-    ->onAnyChange(function (string $type, string $pathString) use ($settings) {
-        $paths = getPaths($pathString, $type);
+  ->onAnyChange(function (string $type, string $pathString) use ($settings) {
+    $paths = getPaths($pathString, $type);
 
-        foreach ($paths as $path) {
-            if(shouldIgnorePath($path, $settings['IGNORED_FOLDERS'])) {
-                continue;
-            }
+    foreach ($paths as $path) {
+      if(shouldIgnorePath($path, $settings['IGNORED_FOLDERS'])) {
+        continue;
+      }
 
-            $path = cleanPath($path, $settings['DIRECTORY']);
-            if (!is_dir($settings['LOG_DIR']))
-            {
-                mkdir($settings['LOG_DIR'],0755, true);
-            }
-            writeLog($path, $type);
-            writeOverview($path);
-        }
-    })
-    ->start();
+      $path = cleanPath($path, $settings['DIRECTORY']);
+      if (!is_dir($settings['LOG_DIR']))
+      {
+        mkdir($settings['LOG_DIR'],0755, true);
+      }
+      writeLog($path, $type);
+      writeOverview($path);
+    }
+  })
+  ->start();
 
 
 /**
@@ -43,13 +43,13 @@ Watch::path($settings['DIRECTORY'])
  * @return string[]
  */
 function getPaths(string $pathString, string $type): array {
-    $paths = [$pathString];
+  $paths = [$pathString];
 
-    if (str_contains($pathString, $type)) {
-        $paths = explode($type . " - ", $pathString);
-    }
+  if (str_contains($pathString, $type)) {
+    $paths = explode($type . " - ", $pathString);
+  }
 
-    return $paths;
+  return $paths;
 }
 
 /**
@@ -59,9 +59,9 @@ function getPaths(string $pathString, string $type): array {
  */
 function shouldIgnorePath(string $path, array $ignoredFolders): bool
 {
-    $pattern = '/[\/\\\\](' . implode('|', array_map('preg_quote', $ignoredFolders)) . ')[\/\\\\]/';
+  $pattern = '/[\/\\\\](' . implode('|', array_map('preg_quote', $ignoredFolders)) . ')[\/\\\\]/';
 
-    return preg_match($pattern, $path) === 1;
+  return preg_match($pattern, $path) === 1;
 }
 
 /**
@@ -71,11 +71,11 @@ function shouldIgnorePath(string $path, array $ignoredFolders): bool
  */
 function cleanPath(string $path, string $directory): string
 {
-    return str_replace(
-        [$directory . '\\', "\n", "\r"],
-        ['', '', ''],
-        $path
-    );
+  return str_replace(
+    [$directory . '\\', "\n", "\r"],
+    ['', '', ''],
+    $path
+  );
 }
 
 /**
@@ -86,11 +86,11 @@ function cleanPath(string $path, string $directory): string
  */
 function writeLog(string $path, string $type): void
 {
-    $log = fopen(FILE_NAME . "_log.txt", "a");
-    fwrite($log, date('H:i:s') . ": {$path} - {$type}\n");
-    fclose($log);
+  $log = fopen(FILE_NAME . "_log.txt", "a");
+  fwrite($log, date('H:i:s') . ": {$path} - {$type}\n");
+  fclose($log);
 
-    echo date('H:i:s') . " - Logged activity", PHP_EOL;
+  echo date('H:i:s') . " - Logged activity", PHP_EOL;
 }
 
 /**
@@ -100,29 +100,37 @@ function writeLog(string $path, string $type): void
  */
 function writeOverview(string $path): void
 {
-    $filename = FILE_NAME . "_overview.txt";
-    $project = explode("\\", $path)[0];
-    $currentTime = date('H:i:s');
+  $filename = FILE_NAME . "_overview.txt";
+  $project = explode("\\", $path)[0];
+  $currentTime = date('H:i:s');
 
-    if (file_exists($filename)) {
-        $lines = file($filename);
-        $last_line = $lines[count($lines)-1];
+  if (file_exists($filename)) {
+    $lines = file($filename);
+    $last_line = $lines[count($lines)-1];
 
-        if(!empty($last_line))
-        {
-            $currentProject = explode("- ", $last_line)[1];
-            $currentProject = str_replace(["\n", "\r"], '', $currentProject);
+    if(!empty($last_line))
+    {
+      $explodeLine = explode(" | ", $last_line);
+      $currentProject = str_replace(["\n", "\r"], '', $explodeLine[1]);
 
-            if ($project === $currentProject)
-            {
-                return;
-            }
-        }
+      if ($project === $currentProject)
+      {
+        return;
+      }
+
+      $completeTime = $explodeLine[0] . " - " . $currentTime;
+      unset($lines[count($lines)-1]);
+      file_put_contents($filename, $lines); 
+
+      $overview = fopen($filename, "a");
+      fwrite($overview, $completeTime . " | {$currentProject}\n");
+      fclose($overview);
     }
+  }
+  $overview = fopen($filename, "a");
+  fwrite($overview, $currentTime . " | {$project}\n");
+  fclose($overview);
 
-    $overview = fopen($filename, "a");
-    fwrite($overview, $currentTime . " - {$project}\n");
-    fclose($overview);
-
-    echo $currentTime . " - Written in overview", PHP_EOL;
+  echo $currentTime . " - Written in overview", PHP_EOL;
 }
+
